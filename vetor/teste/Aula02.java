9bb6fade751ac79c483a5a80cb5ae8c98bd03e98
Marcus Silva <marcusviniferrer@gmail.com>
2023-01-12 11:23:46 -0300

    aula 02: adicionando elementos no vetor

---
+++ vetor/teste/Aula02.java
@@ -0,0 +1,15 @@
+package com.marcus.estruturadados.vetor.teste;
+
+import com.marcus.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+	public static void main(String[]args) {
+		
+		Vetor vetor = new Vetor(5);
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		
+	}
+
+}
