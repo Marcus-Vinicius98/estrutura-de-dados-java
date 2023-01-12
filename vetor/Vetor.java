b89a0f18ff49bc1ad477d00ee534a9890bc17be9
Marcus Silva <marcusviniferrer@gmail.com>
2023-01-12 12:39:30 -0300

    aula 02: adicionando o elemento no vetor com posicao guardada

--- vetor/Vetor.java
+++ vetor/Vetor.java
@@ -3,20 +3,31 @@ package com.marcus.estruturadados.vetor;
 public class Vetor {
 
 	private String[] elementos;
+	private int tamanho;
 
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+		this.tamanho = 0;
 	}
 
-	public void adiciona(String elemento) {
+	/*public void adiciona(String elemento) {
 		for (int i = 0; i < this.elementos.length; i++) {
 			if (this.elementos[i] == null) {
 				this.elementos[i] = elemento;
 				break;
 			}
-
 		}
-
+	}*/
+	
+	public void adiciona(String elemento) throws Exception {
+	  if (this.tamanho < this.elementos.length) {
+		  this.elementos[this.tamanho] = elemento;
+		  this.tamanho++;
+		 } else {
+			 throw new Exception("O vetor já está cherio");
+		 }
 	}
+	
+	
 
 }
--- vetor/teste/Aula02.java
+++ vetor/teste/Aula02.java
@@ -3,13 +3,19 @@ package com.marcus.estruturadados.vetor.teste;
 import com.marcus.estruturadados.vetor.Vetor;
 
 public class Aula02 {
-	public static void main(String[]args) {
-		
-		Vetor vetor = new Vetor(5);
-		
-		vetor.adiciona("elemento 1");
-		vetor.adiciona("elemento 2");
-		
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(3);
+
+		try {
+			vetor.adiciona("elemento 1");
+			vetor.adiciona("elemento 2");
+			vetor.adiciona("elemento 3");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
