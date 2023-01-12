3f68a861105bbe0281f1d163150bacb56ab625e7
Marcus Silva <marcusviniferrer@gmail.com>
2023-01-12 09:53:38 -0300

    aula 01: vetor criado

---
+++ vetor/Vetor.java
@@ -0,0 +1,11 @@
+package com.marcus.estruturadados.vetor;
+
+public class Vetor {
+
+	private String[] elementos;
+
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
+
+}
---
+++ vetor/teste/Aula01.java
@@ -0,0 +1,13 @@
+package com.marcus.estruturadados.vetor.teste;
+
+import com.marcus.estruturadados.vetor.Vetor;
+
+public class Aula01 {
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(5);
+		
+		
+	}
+
+}
