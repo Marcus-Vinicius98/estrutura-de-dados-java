70c2744ac1a4d2da4f47a0e682f6dd9403c7aa2c
Marcus Silva <marcusviniferrer@gmail.com>
2023-01-13 14:59:38 -0300

    aula 03 : tamanho do vetor e imprimindo elementos

---
+++ vetor/teste/Aula03.java
@@ -0,0 +1,21 @@
+package com.marcus.estruturadados.vetor.teste;
+
+import com.marcus.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(10);
+		
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+		
+		
+		System.out.println(vetor.tamanho());
+		
+		System.out.println(vetor.toString());
+
+	}
+
+}
